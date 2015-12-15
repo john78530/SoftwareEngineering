6fd8b1006df17e3b53e0cd528cd247dd20d6d5ab
John <john78530@yahoo.com.tw>
2015-12-15 19:33:17 +0800

    選專案前往需求頁面

---
+++ js/LoginRequestor.js
@@ -0,0 +1,30 @@
+console.log('register');
+var register  = function(){
+
+  var obj = {
+    username:$('#display_name')[0].value,
+     email: $('#email')[0].value,
+     password: $('#password')[0].value
+  }
+  console.log(JSON.stringify(obj));
+  $.ajax({
+   //api的url
+   url: 'http://140.124.181.160:8080/softwareEngineer/accounts',
+   dataType: 'json',
+   contentType: 'application/json',
+   data: JSON.stringify(obj),
+   error: function(err) {
+     console.log(err);
+      // $('#info').html('<p>An error has occurred</p>');
+   },
+   success: function(data) {
+      // var $title = $('<h1>').text(data.talks[0].talk_title);
+      // var $description = $('<p>').text(data.talks[0].talk_description);
+      // $('#info')
+      //    .append($title)
+      //    .append($description);
+      console.log(data);
+   },
+   method: 'post'
+});
+}
