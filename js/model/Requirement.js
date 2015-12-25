978eb2a9b01bdbef4e1c73098bdc04c9282383d7
acemeow <chang.yang.yang@gmail.com>
2015-12-25 17:43:41 +0800

    model prototypes, enter project  BTN bug fixed

---
+++ js/model/Requirement.js
@@ -0,0 +1,12 @@
+function Requirement(id, name, date, type, desc, relatedREQ[], subREQ[]) {
+	var ID;
+	var Name;
+	var Date;
+	var Type
+	var Description;
+	var relatedREQID[];
+	var subRequirement[];
+	
+	
+	
+}
