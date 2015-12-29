0d071454bc0c16899d1b1308a2086b6914cf4dd2
acemeow <chang.yang.yang@gmail.com>
2015-12-29 13:54:41 +0800

    add upload new project function

--- js/projectPage.js
+++ js/projectPage.js
@@ -40,7 +40,100 @@ $(document).on('click', function (e) {
 
 var add_member  = function(){
         console.log('add_member');
-
-
-
+        /////////////////////
+        /////////////////////
 }
+
+console.log('newProject');
+var newProject  = function(_name, _desc){
+  var obj = {
+    name: _name,
+    notes: _desc
+  };
+  console.log(JSON.stringify(obj));
+  $.ajax({
+   //api的url
+   url: "http://140.124.181.160:8080/softwareEngineer/projects",
+   dataType: 'json',
+   contentType: 'application/json',
+   data: JSON.stringify(obj),
+   error: function(err) {
+     console.log(err);
+     $.msgBox({
+      title: "Ooops",
+      content: "ERROR occurred!!!",
+      type: "error",
+      showButtons: false,
+      opacity: 0.9,
+      autoClose:true
+    });
+
+      // $('#info').html('<p>An error has occurred</p>');
+    },
+    success: function(data) {
+      console.log(data);
+    },
+    method: 'post'
+
+  });
+}
+
+
+var add_proj = function(elem){
+    console.log(elem);
+    var name = $("[name='name"+elem.name+"']").val();
+    var desc = $("[name='desc"+elem.name+"']").val();
+    console.log(name+" "+desc);
+    newProject(name, desc);
+    
+}
+
+//console.log('getProjects');
+// var getProjects  = function(){
+
+//   // var obj = {
+//   //   id: localStorage.getItem("id")
+//   // }
+//     var obj = {
+//     id: $('#username')[0].value,
+//     name: $('#password')[0].value,
+//   }
+//   console.log(JSON.stringify(obj));
+//   $.ajax({
+//    //api的url
+//    url: 'http://140.124.181.160:8080/softwareEngineer/projects',
+//    dataType: 'json',
+//    contentType: 'application/json',
+//    data: JSON.stringify(obj),
+//    error: function(err) {
+//      console.log(err);
+//      $.msgBox({
+//       title: "Ooops",
+//       content: "ERROR occurred!!!",
+//       type: "error",
+//       showButtons: false,
+//       opacity: 0.9,
+//       autoClose:true
+//     });
+
+//       // $('#info').html('<p>An error has occurred</p>');
+//     },
+//     success: function(data) {
+//       // var $title = $('<h1>').text(data.talks[0].talk_title);
+//       // var $description = $('<p>').text(data.talks[0].talk_description);
+//       // $('#info')
+//       //    .append($title)
+//       //    .append($description);
+//       localStorage.setItem("id", data.id);
+//       console.log(data);
+//       location.href = "http://john78530.github.io/project.html";      
+//     },
+//     method: 'post'
+
+//   });
+// }
+
+
+
+
+
